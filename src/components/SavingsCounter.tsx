340f3a0a1ce42c4b194af312b58dbd1fc3742e73
fnstggl <fnstggl@gmail.com>
2025-09-26 16:19:40 -0400

    Update SavingsCounter.tsx

--- src/components/SavingsCounter.tsx
+++ src/components/SavingsCounter.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
 const SavingsCounter = () => {
-  const [totalSaved, setTotalSaved] = useState(98351021);
+  const [totalSaved, setTotalSaved] = useState(48351021);
   const [isMobile, setIsMobile] = useState(false);
 
   // Mobile detection
